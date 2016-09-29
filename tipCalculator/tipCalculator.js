82112a74c9a45dbec1c550ffe6d20375ac3540f1
Riley Osborne <mitchell.osborne@interapt.hq>
2016-09-29 09:40:23 -0400

    Initial commit

---
+++ tipCalculator/tipCalculator.js
@@ -0,0 +1,44 @@
+  function validate(evt) {
+  var theEvent = evt || window.event;
+  var key = theEvent.keyCode || theEvent.which;
+  key = String.fromCharCode(key);
+  var regex = /[0-9]|\./;
+  if (!regex.test(key)) {
+      theEvent.returnValue = false;
+      if (theEvent.preventDefault) theEvent.preventDefault();
+  }
+}
+function calculate(){
+  var billAmount = $("#bill").val();
+  var serviceQuality = $('#service').val();
+  var numPeople = $('#people').val();
+  if (billAmount == "" || serviceQuality == 0){
+   window.alert("Please enter values");
+    return;
+  }
+  var tip = (billAmount * serviceQuality)/ numPeople;
+  tip = Math.round(tip * 100) / 100;
+  tip = tip.toFixed(2);
+$('#tip').text(tip);
+var total = (parseInt(billAmount) + parseInt(tip))/ numPeople;
+$('#total').text(total);
+  }
+function clear(){
+  $("#bill").val(" ");
+  $('#service').val(" ");
+  $('#people').val(" ");
+  $('#tip').text(" ");
+
+
+}
+
+$(function (){ 
+ $('#calculate').on("click", function() {
+    calculate();
+  
+  });
+  /*$('#clear').on("click", function(){
+    clear();
+  });*/
+
+});
